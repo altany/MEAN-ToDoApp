4b0203cb8905f374115cfb70f122bbb2cd590b01
Tania <taniapapazaf@gmail.com>
2015-12-22 15:24:42 +0000

    Set the port

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.set('port', (process.env.PORT || 5000));
+
 // set the favicon
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
